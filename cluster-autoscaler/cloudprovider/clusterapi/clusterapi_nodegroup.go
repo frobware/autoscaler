ef305b66544a26c3389a3257e4408e85d7b72a2f
Andrew McDermott <amcdermo@redhat.com>
2019-06-03 11:38:36 +0100

    Prioritize node/machine linkage using ProviderID
    This is a merge https://github.com/openshift/kubernetes-autoscaler/pull/100

--- cluster-autoscaler/cloudprovider/clusterapi/clusterapi_nodegroup.go
+++ cluster-autoscaler/cloudprovider/clusterapi/clusterapi_nodegroup.go
@@ -100,7 +100,7 @@ func (ng *nodegroup) DeleteNodes(nodes []*corev1.Node) error {
 			return fmt.Errorf("node %q doesn't belong to node group %q", node.Spec.ProviderID, ng.Id())
 		}
 
-		machine, err := ng.machineController.findMachineByNodeProviderID(node)
+		machine, err := ng.machineController.findMachineByProviderID(node.Spec.ProviderID)
 		if err != nil {
 			return err
 		}
